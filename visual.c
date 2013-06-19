71b0aacabd2076836ba85a5ab565f7677180fa23
mahvili <mahyar.valizadeh@gmail.com>
2013-06-19 19:56:21 +0200

    visual

--- visual.c
+++ visual.c
@@ -3,22 +3,28 @@
 #include <stdio.h>
 
 
-void write_vtkFile(const char *szProblem,
-                   int    timeStepNumber,
-                   double xlength,
-                   double ylength,
-                   int    imax,
-                   int    jmax,
-                   double dx,
-                   double dy,
-                   double **U,
-                   double **V,
-                   double **P) {
+void output_uvp(
+                double **U,
+                double **V,
+                double **P,
+                int il,
+                int ir,
+                int jb,
+                int jt,
+                int omg_i,
+                int omg_j,
+                char *szProblem,
+                int timeStepNumber,
+                double dx,
+                double dy
+                ){
+    
+
     
     int i,j;
     char szFileName[80];
     FILE *fp=NULL;
-    sprintf( szFileName, "%s.%i.vtk", szProblem, timeStepNumber );
+    sprintf( szFileName, "%s.%i.%i_%i.vtk", szProblem, timeStepNumber, omg_i, omg_j  );
     fp = fopen( szFileName, "w");
     if( fp == NULL )
     {
@@ -28,25 +34,25 @@ void write_vtkFile(const char *szProblem,
         return;
     }
     
-    write_vtkHeader( fp, imax, jmax, dx, dy);
-    write_vtkPointCoordinates(fp, imax, jmax, dx, dy);
+    write_vtkHeader( fp, il, ir, jb, jt, dx, dy);
+    write_vtkPointCoordinates(fp, il, ir, jb, jt, dx, dy);
     
-    fprintf(fp,"POINT_DATA %i \n", (imax+1)*(jmax+1) );
+    fprintf(fp,"POINT_DATA %i \n", (ir-(il-1)+1)*(jt-(jb-1)+1) );
 	
     fprintf(fp,"\n");
     fprintf(fp, "VECTORS velocity float\n");
-    for(j = 0; j < jmax+1; j++) {
-        for(i = 0; i < imax+1; i++) {
+    for(j = jb-1; j <=jt; j++) {
+        for(i = il-1; i <=ir; i++) {
             fprintf(fp, "%f %f 0\n", (U[i][j] + U[i][j+1]) * 0.5, (V[i][j] + V[i+1][j]) * 0.5 );
         }
     }
     
     fprintf(fp,"\n");
-    fprintf(fp,"CELL_DATA %i \n", ((imax)*(jmax)) );
+    fprintf(fp,"CELL_DATA %i \n", ((ir-il+1)*(jt-jb+1)) );
     fprintf(fp, "SCALARS pressure float 1 \n");
     fprintf(fp, "LOOKUP_TABLE default \n");
-    for(j = 1; j < jmax+1; j++) {
-        for(i = 1; i < imax+1; i++) {
+    for(j = jb; j <= jt; j++) {
+        for(i = il; i <= ir; i++) {
             fprintf(fp, "%f\n", P[i][j] );
         }
     }
@@ -60,8 +66,15 @@ void write_vtkFile(const char *szProblem,
 }
 
 
-void write_vtkHeader( FILE *fp, int imax, int jmax,
-                     double dx, double dy) {
+void write_vtkHeader(
+                     FILE *fp,
+                     int il,
+                     int ir,
+                     int jb,
+                     int jt,
+                     double dx,
+                     double dy
+                     ) {
     if( fp == NULL )
     {
         char szBuff[80];
@@ -75,22 +88,28 @@ void write_vtkHeader( FILE *fp, int imax, int jmax,
     fprintf(fp,"ASCII\n");
     fprintf(fp,"\n");
     fprintf(fp,"DATASET STRUCTURED_GRID\n");
-    fprintf(fp,"DIMENSIONS  %i %i 1 \n", imax+1, jmax+1);
-    fprintf(fp,"POINTS %i float\n", (imax+1)*(jmax+1) );
+    fprintf(fp,"DIMENSIONS  %i %i 1 \n", ir-(il-1)+1, jt-(jb-1)+1);
+    fprintf(fp,"POINTS %i float\n", (ir-(il-1)+1)*(jt-(jb-1)+1) );
     fprintf(fp,"\n");
 }
 
 
-void write_vtkPointCoordinates( FILE *fp, int imax, int jmax,
-                               double dx, double dy) {
-    double originX = 0.0;  
-    double originY = 0.0;
+void write_vtkPointCoordinates(
+                               FILE *fp,
+                               int il,
+                               int ir,
+                               int jb,
+                               int jt,
+                               double dx,
+                               double dy) {
+    double originX = (il-1)*dx;
+    double originY = (jb-1)*dy;
     
     int i = 0;
     int j = 0;
     
-    for(j = 0; j < jmax+1; j++) {
-        for(i = 0; i < imax+1; i++) {
+    for(j = 0; j <= jt-(jb-1); j++) {
+        for(i = 0; i <=ir-(il-1); i++) {
             fprintf(fp, "%f %f 0\n", originX+(i*dx), originY+(j*dy) );
         }
     }
